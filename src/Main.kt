08094586870401c664f6159cabff113725bbc763
nickelk <nick_smith@ukr.net>
2020-09-06 07:09:18 +0300

    data classes added

--- src/Main.kt
+++ src/Main.kt
@@ -11,9 +11,20 @@ class Some {
 }
 
 fun main(args: Array<String>) {
+
+    val db = Db.MongoDb(5, "mongo")
+    val db2 = Db.PostgreSQL(6, "postgre", true)
+
+    val dbCopy = db.copy(id = 7, conn = "done")
+    if(db == dbCopy) println("They are equal")
+    else println("They are not equal")
+
+    if(db is Db.MongoDb){
+        db.printInfo()
+    }
     //Some.count = 5
-    val some = Some()
-    val some_2 = Some()
-    val some_3 = Some()
-    val some_4 = Some()
+//    val some = Some()
+//    val some_2 = Some()
+//    val some_3 = Some()
+//    val some_4 = Some()
 }
--- src/SealedClass.kt
+++ src/SealedClass.kt
@@ -1,5 +1,19 @@
 sealed class Db {
     data class MySQL(val id: Int, val conn: String): Db()
-    data class MongoDb(val id: Int, val conn: String): Db()
+    data class MongoDb(val id: Int, val conn: String): Db(){
+//        fun printInfo(){
+//            println("MongoDb has id: $id and connection: $conn")
+  //      }
+    }
     data class PostgreSQL(val id: Int, val conn: String, val isDone: Boolean): Db()
+    object Help : Db(){
+        val conn = "Connection done"
+    }
+}
+
+val Db.MongoDb.info: String
+    get() = "MongoDb has id: $id and connection: $conn"
+
+fun Db.MongoDb.printInfo() {
+    println(info)
 }
