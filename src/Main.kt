96db62b820800e7d2bcbc9ae3e6ce4729c7984ae
nickelk <nick_smith@ukr.net>
2020-09-06 08:14:39 +0300

    functions added

--- src/Main.kt
+++ src/Main.kt
@@ -10,18 +10,32 @@ class Some {
     }
 }
 
-fun main(args: Array<String>) {
-
-    val db = Db.MongoDb(5, "mongo")
-    val db2 = Db.PostgreSQL(6, "postgre", true)
-
-    val dbCopy = db.copy(id = 7, conn = "done")
-    if(db == dbCopy) println("They are equal")
-    else println("They are not equal")
-
-    if(db is Db.MongoDb){
-        db.printInfo()
+fun filterList(list: List<String>, filter: (String) -> Boolean){
+    list.forEach { el ->
+        if (filter(el)) println(el)
     }
+}
+
+val filter: (String) -> Boolean ={
+    it.startsWith("P")
+}
+
+fun main(args: Array<String>) {
+    val list = listOf("Java", "PHP", "Perl", "JavaScript", "C++")
+    filterList(list, filter) // { it.startsWith("P") })
+
+//    val db = Db.MongoDb(5, "mongo")
+//    val db2 = Db.PostgreSQL(6, "postgre", true)
+//
+//    val dbCopy = db.copy(id = 7, conn = "done")
+//    if(db == dbCopy) println("They are equal")
+//    else println("They are not equal")
+//
+//    if(db is Db.MongoDb){
+//        db.printInfo()
+//    }
+
+
     //Some.count = 5
 //    val some = Some()
 //    val some_2 = Some()
